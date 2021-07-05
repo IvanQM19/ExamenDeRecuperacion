40ef8ad25fc09e15405cac6c3beee70ed2fafe6a
IvanQM19 <85361546+IvanQM19@users.noreply.github.com>
2021-07-05 10:37:03 -0500

    Update ExamenSegundoEjercicioO.m

--- ExamenSegundoEjercicioO.m
+++ ExamenSegundoEjercicioO.m
@@ -1,1 +1,13 @@
-pkg load symbolic % Nombre: Jose Ivan Quijas Miranda% Correo: ivanqm@outlook.es% Carrera: Ingenieria en sistemas computacionales % Grupo: 3202 syms x; f = (x.^5)+(2x.^2)+3);a = 1;b = 6;int(f, a, b)ezplot(f)
+pkg load symbolic 
+% Nombre: Jose Ivan Quijas Miranda
+% Correo: ivanqm@outlook.es
+% Carrera: Ingenieria en sistemas computacionales 
+% Grupo: 3202 
+
+syms x; 
+f = (x.^5)+(2x.^2)+3);
+a = 1;
+b = 6;
+
+int(f, a, b)
+ezplot(f)
